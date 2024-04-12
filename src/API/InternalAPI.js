e2d6b5a25ffd0ff68b5e348dd518aadde1c0ba12
Nikidzawa <datr1932@gmail.com>
2024-04-12 16:16:42 +0500

    update api

--- src/API/InternalAPI.js
+++ src/API/InternalAPI.js
@@ -1,16 +1,16 @@
 export default class InternalAPI {
     static getUser (userId) {
-        const getUserURL = `http://localhost:8080/api/getUserById/${userId}`
+        const getUserURL = `https://nikidzawa.online/api/getUserById/${userId}`
         return fetch(getUserURL);
     }
 
     static getRole (userId) {
-        const getUserURL = `http://localhost:8080/api/getUserStatus/${userId}`
+        const getUserURL = `https://nikidzawa.online/api/getUserStatus/${userId}`
         return fetch(getUserURL).then(response => response.text());
     }
     static postEvent(event) {
         const userId = localStorage.getItem("userId");
-        const getUserRoleURL = `http://localhost:8080/api/postEvent/${userId}`;
+        const getUserRoleURL = `https://nikidzawa.online/api/postEvent/${userId}`;
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -20,13 +20,13 @@ export default class InternalAPI {
     }
 
     static getEvent (eventId) {
-        const getUserEventsUrl = `http://localhost:8080/api/getEvent/${eventId}`;
+        const getUserEventsUrl = `https://nikidzawa.online/api/getEvent/${eventId}`;
         return fetch(getUserEventsUrl);
     }
 
     static deleteEventCityById (eventId) {
         const userId = localStorage.getItem("userId");
-        const url = `http://localhost:8080/api/deleteEventById/${eventId}/${userId}`;
+        const url = `https://nikidzawa.online/api/deleteEventById/${eventId}/${userId}`;
         const requestOptions = {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json' },
@@ -35,18 +35,18 @@ export default class InternalAPI {
     }
 
     static getEventsByType (typeId) {
-        const url = `http://localhost:8080/api/getEventsByType/${typeId}`
+        const url = `https://nikidzawa.online/api/getEventsByType/${typeId}`
         return fetch(url).then(response => response.json());
     }
 
     static getAllEventTypes () {
-        const url = `http://localhost:8080/api/getAllEventTypes`;
+        const url = `https://nikidzawa.online/api/getAllEventTypes`;
         return fetch(url).then(response => response.json());
     }
 
     static deleteEventTypeById (id) {
         const userId = localStorage.getItem("userId");
-        const url = `http://localhost:8080/api/deleteEventType/${id}/${userId}`;
+        const url = `https://nikidzawa.online/api/deleteEventType/${id}/${userId}`;
         const requestOptions = {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json' },
@@ -56,7 +56,7 @@ export default class InternalAPI {
 
     static postEventType (eventType) {
         const userId = localStorage.getItem("userId");
-        const postEventType = `http://localhost:8080/api/postEventType/${userId}`;
+        const postEventType = `https://nikidzawa.online/api/postEventType/${userId}`;
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -66,18 +66,18 @@ export default class InternalAPI {
     }
 
     static getEventsByCityId (cityId) {
-        const url = `http://localhost:8080/api/getEventsByCityId/${cityId}`
+        const url = `https://nikidzawa.online/api/getEventsByCityId/${cityId}`
         return fetch(url).then(response => response.json());
     }
 
     static getFavouriteEventsByCityId (cityId) {
-        const url = `http://localhost:8080/api/getFavouriteEventsByCityId/${cityId}`
+        const url = `https://nikidzawa.online/api/getFavouriteEventsByCityId/${cityId}`
         return fetch(url).then(response => response.json());
     }
 
     static setOrRemoveEventFavorite (cityId, eventId) {
         const userId = localStorage.getItem("userId");
-        const url = `http://localhost:8080/api/setOrRemoveEventFavorite/${cityId}/${eventId}/${userId}`;
+        const url = `https://nikidzawa.online/api/setOrRemoveEventFavorite/${cityId}/${eventId}/${userId}`;
         const requestOptions = {
             method: 'PATCH',
             headers: { 'Content-Type': 'application/json' },
@@ -86,13 +86,13 @@ export default class InternalAPI {
     }
 
     static getAllCities () {
-        const url = `http://localhost:8080/api/getEventCities`
+        const url = `https://nikidzawa.online/api/getEventCities`
         return fetch(url).then(response => response.json());
     }
 
     static postEventCity (eventCity) {
         const userId = localStorage.getItem("userId");
-        const url = `http://localhost:8080/api/postEventCity/${userId}`
+        const url = `https://nikidzawa.online/api/postEventCity/${userId}`
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
@@ -103,7 +103,7 @@ export default class InternalAPI {
 
     static deleteEventCityById (eventCityId) {
         const userId = localStorage.getItem("userId");
-        const url = `http://localhost:8080/api/deleteEventCity/${eventCityId}/${userId}`;
+        const url = `https://nikidzawa.online/api/deleteEventCity/${eventCityId}/${userId}`;
         const requestOptions = {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json' },
